31036687615c78cd7d9f548ab92a7ef4f9259089
richardkefa <richardkefa7@gmail.com>
2021-07-07 23:32:13 +0300

    add modified items model and order model annd sell function view file

--- orders/models.py
+++ orders/models.py
@@ -1,19 +1,21 @@
 from django.db import models
-
-from django.db import  models
-
-
 class Items(models.Model):
     item_name = models.CharField(max_length=100)
-    item_qty = models.IntegerField(default=1)
-    item_sold = models.BooleanField()
+    item_qty = models.IntegerField(default=0)
     item_entry_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.item_name
 
+    def save_item(self):
+        return self.save()
+
 
 class Order(models.Model):
+    item_name = models.ForeignKey(Items,on_delete=models.CASCADE)
     order_no = models.IntegerField(auto_created=True)
     Date_of_order = models.DateTimeField(auto_now_add=True)
-    dispached = models.BooleanField()
+    dispached = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.order_no
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Items,Order
 
-# Create your views here.
+def items_lIst(request):
+    item_list =Items.objects.all()
+    context = {
+        'item_list':item_list,
+    }
+    return render(request,'index.html',context)
+
+def sell_item(request,id):
+    item = Items.get(id)
+    quantity = item.quantity
+    new_quantity = quantity-1
+    Items.quantity = new_quantity
+    Items.save()
+
+    context = {
+        "new_qeantity":quantity
+    }
+    return render(request,'index.html',quantity)
