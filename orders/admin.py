d170ecc14efab429f5b64b7d7ba5cd22ddf990d5
richardkefa <richardkefa7@gmail.com>
2021-07-08 00:38:56 +0300

    add  psql database and configuration, created superuser and admin page

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Items,Order
 
-# Register your models here.
+admin.site.register(Items)
+admin.site.register(Order)
--- reorder/settings.py
+++ reorder/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'orders',
 ]
 
 MIDDLEWARE = [
@@ -77,12 +80,28 @@ WSGI_APPLICATION = 'reorder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('MODE')=="dev":
+    DATABASES = {
+        'default': {
+            'ENGINE':'django.db.backends.postgresql',
+            'NAME':os.getenv('DB_NAME'),
+            'USER':os.getenv('DB_USER'),
+            'PASSWORD':os.getenv('DB_PASSWORD'),
+            'HOST':os.getenv('DB_HOST'),
+            'PORT':'',
+        }
+        
     }
-}
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=os.getenv('DATABASE_URL')
+       )
+   }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
